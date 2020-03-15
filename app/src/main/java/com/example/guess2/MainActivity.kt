5e749cf4e6bf136a8e36f97a20378659e78fe896
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-03-15 08:52:43 +0800

    課程10 LinearLayout 與元件的分布屬性

--- app/src/main/java/com/example/guess2/MainActivity.kt
+++ app/src/main/java/com/example/guess2/MainActivity.kt
@@ -10,6 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 //        複寫 副類別 onCreate自動執行類別
     super.onCreate(savedInstanceState)
+    /*        setContentView(R.layout.linear_main)
+                linear_main.xml 封面顯示為此畫面
+        * */
         setContentView(R.layout.activity_main)
     }
 }
