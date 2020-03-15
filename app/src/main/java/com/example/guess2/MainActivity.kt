917998b327d00f1c0d51bd5f7c076ecd19394359
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-03-15 10:11:31 +0800

    課程14 抽取字串資源、如何讓app擁有多國語言實例

--- app/src/main/java/com/example/guess2/MainActivity.kt
+++ app/src/main/java/com/example/guess2/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
 //建立MainActivity 繼承 AppCompatActivity()
     val secretNumber=SecretNumber()
 //    產生可以用secretNumber的類別方法
+    val TAG = MainActivity::class.java.simpleName
+//    定義TAG = MainActivity的類別名稱
     override fun onCreate(savedInstanceState: Bundle?) {
 //        複寫 副類別 onCreate自動執行類別
     super.onCreate(savedInstanceState)
@@ -19,7 +21,7 @@ class MainActivity : AppCompatActivity() {
                 linear_main.xml 封面顯示為此畫面
         * */
         setContentView(R.layout.activity_main)
-    Log.d("MainActivity", "secret:"+secretNumber.secert);
+    Log.d(TAG, "secret:"+secretNumber.secert);
 //    Log.d (secretNumber) 提示出secert數字
     }
     fun check (view:View)
@@ -29,28 +31,30 @@ class MainActivity : AppCompatActivity() {
 //      定義一個n = number為text的字串轉數字
         println("number:${n}")
 //       可使用println簡單打印出文字
-        Log.d("MainActivity","number:"+n)
+        Log.d(TAG,"number:"+n)
 //        Log除錯專用的d (tag是""哪個地方出來的  ,  msg是將""訊息印出來)\
         val diff = secretNumber.validate(n)
-        var message = "Yes you got it"
+//        定義diff為secretNumber.validate(n)
+        var message = getString(R.string.yes_you_got_it)
+//        定義message 為""裡面的文字
         if (secretNumber.validate(n)<0) {
 //      判斷式if secretNumber裡面的validate方法 ,把n船進去是否<0
-        Toast.makeText(this,"Bigger",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,R.string.bigger,Toast.LENGTH_SHORT).show()
 //      提示訊息Toast 印出make裡面的文字(context為位置,text為文字內容,顯示為短暫)顯示出來
-        message="Bigger"
+        message=getString(R.string.bigger)
 //      簡化提示訊息
         }else if (diff>0){
-        message = "Smaller"
+        message = getString(R.string.smaller)
 //      簡化提示訊息
-        }else{Toast.makeText(this,"Yes you got it",Toast.LENGTH_SHORT).show()
+        }else{Toast.makeText(this,getString(R.string.yes_you_got_it),Toast.LENGTH_SHORT).show()
 //      正常if else 用法以及Toast正常用法
         }
             Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
 //簡化提示訊息及else
     AlertDialog.Builder(this)
-        .setTitle("Message")
+        .setTitle(getString(R.string.dialog_title))
         .setMessage(message)
-        .setPositiveButton("OK",null)
+        .setPositiveButton(getString(R.string.ok),null)
         .show()
 //      對話框的Builder模式(在哪顯示)標題、訊息、按鈕("文字內容",之後不做任何事)、顯示
     }
