1658ddbf7e48a5f898619cee53502124a1c9d0f2
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-03-15 09:08:40 +0800

    課程11 程式中存取元件、輸入方塊、按鈕元件互動程式設計

--- app/src/main/java/com/example/guess2/MainActivity.kt
+++ app/src/main/java/com/example/guess2/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.guess2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //建立MainActivity 繼承 AppCompatActivity()
@@ -14,5 +17,15 @@ class MainActivity : AppCompatActivity() {
                 linear_main.xml 封面顯示為此畫面
         * */
         setContentView(R.layout.activity_main)
+    }
+    fun check (view:View)
+//    建立一個check方法 (view物件:View類別)
+    {
+        val n = number.text.toString().toInt()
+//      定義一個n = number為text的字串轉數字
+        println("number:${n}")
+//       可使用println簡單打印出文字
+        Log.d("MainActivity","number:"+n)
+//        Log除錯專用的d (tag是""哪個地方出來的  ,  msg是將""訊息印出來)
     }
 }
